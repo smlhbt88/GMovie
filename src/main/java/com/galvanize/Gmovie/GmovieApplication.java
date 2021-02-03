aadb7168c5254321cf700783ee1f63e3d52bbae5
smlhbt88 <samuel.habte@cognizant.com>
2021-02-03 08:04:49 -0800

    initial commit

---
+++ src/main/java/com/galvanize/Gmovie/GmovieApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.Gmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/Gmovie/GmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.Gmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
